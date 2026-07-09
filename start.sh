60b8e17b4deac70c9a1fdccdb54e6fcf4a620553
Thorin-56 <118696034+Thorin-56@users.noreply.github.com>
2026-07-09 11:44:56 +0200

    Remove unnecessary memory allocation flag in start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo -e "=== Vérification des mises à jour Paper ==="
 bash update.sh
 
 echo -e "=== Démarrage du serveur ==="
-exec java -Xms128M --XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar ${SERVER_JARFILE} nogui
+exec java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar ${SERVER_JARFILE} nogui
